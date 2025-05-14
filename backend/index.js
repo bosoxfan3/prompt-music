dc21ed87c5989c422b8d7eb2f151867a1e2a0439
Daniel Acquesta <dgacquesta@gmail.com>
2025-05-14 12:06:59 -0700

    fix the port issue

--- backend/index.js
+++ backend/index.js
@@ -246,6 +246,6 @@ app.post('/create-playlist', async (req, res) => {
     }
 });
 
-app.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+    console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
